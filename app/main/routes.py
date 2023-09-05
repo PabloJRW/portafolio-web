7ec1af56349298114cefc52e956393238e89bd16
Pablo <ramoswilkinspj28@gmail.com>
2023-09-05 12:06:45 -0500

    contenido proyecto1

--- app/main/routes.py
+++ app/main/routes.py
@@ -2,6 +2,14 @@ from flask import render_template, request
 from app.main import main_bp
 
 
+# Página principal
 @main_bp.route('/')
 def index():
     return render_template('main/index.html')
+
+
+
+# Proyecto 1
+@main_bp.route('/proyecto1')
+def proyecto_1():
+    return render_template('proyectos/api-rest.html')
